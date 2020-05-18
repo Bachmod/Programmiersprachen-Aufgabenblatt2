3be8342fdfa830793c7e3f8e3d88d108c2037183
bachmod <lliw.strong@yahoo.de>
2020-05-18 14:08:33 +0200

    blub

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,9 @@
 #define CATCH_CONFIG_RUNNER
+#include <iostream>
 #include <catch.hpp>
 
 int main(int argc, char *argv[])
 {
+    std::cout << "hello world";
   return Catch::Session().run(argc, argv);
 }
